dd56d0950216a7e5f02cf9dfc7e8a4e19ed21b1f
dilipBadal <dilipbadal33@gmail.com>
2024-02-09 19:41:26 +0530

    fixed typing

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -1,37 +1,8 @@
 import React, { useState, useEffect } from "react";
+import Typewriter from "../typing";
 import { Box } from "@chakra-ui/react";
 
 function Homepage() {
-  const [introList, setIntroList] = useState([
-    "hi",
-    "Hi",
-    "Hi I am",
-    "Hi I am A",
-    "Hi I am a software",
-    "Hi I am a Software",
-    "Hi I am a Software Developer",
-    "Hi I am a Software Developer",
-  ]);
-
-  const [currentText, setCurrentText] = useState("");
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      const text = introList[currentIndex];
-      setCurrentText((prevText) => prevText + text.charAt(prevText.length));
-      if (currentText === text) {
-        clearInterval(intervalId);
-        setTimeout(() => {
-          setCurrentText("");
-          setCurrentIndex((prevIndex) => (prevIndex + 1) % introList.length);
-        }, 1000);
-      }
-    }, 100);
-
-    return () => clearInterval(intervalId);
-  }, [currentIndex, introList, currentText]);
-
   return (
     <Box
       bg={"primary"}
@@ -41,7 +12,7 @@ function Homepage() {
       height={"100vh"}
       color={"#fff"}
     >
-      <Box>{currentText}</Box>
+      <Box><Typewriter text="Hi, I am Dilip. I'm a Software Developer."  delay={100} /></Box>
     </Box>
   );
 }
